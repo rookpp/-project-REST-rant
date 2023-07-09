f85d140c87442e39c6392f30ffb29179c9fe7f35
Kamil Holub <kahol20000@gmail.com>
2023-07-09 10:01:41 -0400

    Adding 404 page route

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 const express = require('express')
 const app = express()
-
+require('dotenv').config()
 app.get('/', (req, res) => {res.send('hello world!')})
 
-app.listen(3000)
+
+app.get('*',(req, res) => {
+    res.status(404)('<h1>It seems this page does not exist</h1>')
+})
+app.listen(process.env.PORT)
+

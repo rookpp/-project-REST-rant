06d2f62f6ff742714918a3d8ee41f30e732f87ea
Kamil Holub <kahol20000@gmail.com>
2023-07-27 17:18:26 -0400

    Added edit form

--- views/places/index.jsx
+++ views/places/index.jsx
@@ -1,19 +1,24 @@
 const React = require('react')
 function index(data) {
-    let placesFormatted = data.places.map((place) => {
+    let placesFormatted = data.places.map((place, index) => {
         return (
             <div className="col-sm-6">
-                <h2><a href={`/places/${index}`}>{place.name}</a></h2>
+                <h2>
+                    <a href={`/places/${index}`} >
+                        {place.name}
+                    </a>
+                </h2>
                 <p className="text-center">
                     {place.cuisines}
                 </p>
-                <img src={place.pic} alt={place.name}></img>
+                <img src={place.pic} alt={place.name} />
                 <p className="text-center">
                     Located in {place.city}, {place.state}
                 </p>
             </div>
         )
     })
+
     return (
         <main>
             <h1>Index Page</h1>

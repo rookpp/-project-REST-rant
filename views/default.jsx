140bc1de7f41ba93afa0712254aa5755b79f825a
Kamil Holub <kahol20000@gmail.com>
2023-07-27 12:26:24 -0400

    added navbar

--- views/default.jsx
+++ views/default.jsx
@@ -1,17 +1,30 @@
 const React = require('react')
-function Def (html) {
+function Def(html) {
     return (
         <html>
             <head>
                 <title>Page</title>
                 <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous"></link>
-                <link rel="stylesheet" href="/css/style.css"/>  
+                <link rel="stylesheet" href="/css/style.css" />
             </head>
             <body>
+                <nav>
+                    <ul>
+                        <li>
+                            <a href='/'>home</a>
+                        </li>
+                        <li>
+                            <a href='/places'>Places</a>
+                        </li>
+                        <li>
+                            <a href='places/new'>Add Place</a>
+                        </li>
+                    </ul>
+                </nav>
                 {html.children}
             </body>
         </html>
     )
-  }
-  
+}
+
 module.exports = Def
